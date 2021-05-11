d83a78f35d542bcd1dd1c0d35e801f714c023dd7
tywe <tywe@kth.se>
2021-05-11 21:59:08 +0200

    Changed size of brick

--- src/Brick.java
+++ src/Brick.java
@@ -6,6 +6,6 @@ public class Brick{
     //TODO: build bricks
     public void draw(Graphics g) {
         g.setColor(Color.green);
-        g.fillRect(100,200,20,30);
+        g.fillRect(100,200,200,300);
     }
 }
